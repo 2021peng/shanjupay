0ff74727ce6a43f1ba4ed04749bd2cd44071e74b
2021peng <2285070166@qq.com>
2023-02-05 20:51:19 +0800

    chapter3-2.微信接入（未完成）

---
+++ shanjupay-transaction/shanjupay-transaction-service/src/main/java/com/shanjupay/transaction/controller/WxPayController.java
@@ -0,0 +1,16 @@
+package com.shanjupay.transaction.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+
+/**
+ * @Author: admin
+ * @Date: 2023/2/5 19:44
+ * @Description:
+ */
+
+@Slf4j
+@Controller
+public class WxPayController {
+
+}
