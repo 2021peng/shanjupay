b9b1d3697f139cfc358b1381cfcc3a09c1661d76
2021peng <88758493+2021peng@users.noreply.github.com>
2023-03-15 11:42:18 +0800

    Add files via upload
    add other files

---
+++ itcast-mp-springboot/src/main/java/cn/itcast/mp/pojo/User.java
@@ -0,0 +1,14 @@
+package cn.itcast.mp.pojo;
+
+/**
+ * @BelongsProject: itcast-mp-springboot
+ * @BelongsPackage: cn.itcast.mp.pojo
+ * @Classname: User
+ * @Author: admin
+ * @Date: 2022/12/6 17:11
+ * @Description:
+ */
+
+@TableName("tb_user")
+public class User {
+}
